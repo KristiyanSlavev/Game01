398920a4a6041bb928ccfc2228c98f8f6331f0ba
Kristiyan Slavev <kristiqnslavev97@gmail.com>
2019-10-28 16:49:14 +0200

    Player Health functionality and Death

--- Scripts/HeartManager.cs
+++ Scripts/HeartManager.cs
@@ -11,6 +11,7 @@ public class HeartManager : MonoBehaviour
     public Sprite halfFullHeart;
     public Sprite emptyHeart;
     public FloatValue heartContainers;
+    public FloatValue playerCurrentHealth;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +26,30 @@ public class HeartManager : MonoBehaviour
             //Make hearts active and set them full
             hearts[i].gameObject.SetActive(true);
             hearts[i].sprite = fullHeart;
+        }
+    }
+
+    public void UpdateHearts()
+    {
+        //hearts and halves
+        float tempHealth = playerCurrentHealth.RuntimeValue / 2;
+        //loop over all heart containers and compare; 
+        for(int i =0; i<heartContainers.initialValue; i++)
+        {
+            if (i <= tempHealth-1)
+            {
+                //full Heart
+                hearts[i].sprite = fullHeart;
+            } else if (i >= tempHealth)
+            {
+                //empty Heart
+                hearts[i].sprite = emptyHeart;
+            }
+            else
+            {
+                //half full Hear
+                hearts[i].sprite = halfFullHeart;
+            }
         }
     }
 }
--- Scripts/Scriptable Objects/FloatValue.cs
+++ Scripts/Scriptable Objects/FloatValue.cs
@@ -3,7 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class FloatValue : ScriptableObject
+public class FloatValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public float initialValue;
+
+    [HideInInspector]
+    public float RuntimeValue;
+
+
+    //Load and unload objects; For the whole program
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = initialValue;
+    }
+
+    public void OnBeforeSerialize()
+    {
+
+    }
 }

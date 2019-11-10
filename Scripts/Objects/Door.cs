b40e05c9f44957496b5fda1bc5f6243a3d5dba8c
Kristiyan Slavev <kristiqnslavev97@gmail.com>
2019-11-10 13:36:43 +0000

    Added Dunge0n

---
+++ Scripts/Objects/Door.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public enum DoorType
+{
+    key,
+    enemy,
+    button
+}
+
+//ol(OL
+public class Door : Interactable
+{
+    [Header("Door variables")]
+    public DoorType thisDoorType;
+    public bool open= false;
+    public Inventory playerInventory;
+    public SpriteRenderer doorSprite;
+    public BoxCollider2D physicsCollider;
+
+    //d0ne if the trigger is part 0f the sprite
+    /*private void Start()
+    {
+        doorSprite = GetComponent<SpriteRenderer>();
+    }*/
+
+
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Sword1"))
+        {
+            if (playerInRange && thisDoorType == DoorType.key)
+            {
+                if (playerInventory.numberOfKeys > 0)
+                {
+                    playerInventory.numberOfKeys--;
+                    Open();
+                }
+            }
+        }
+    }
+
+    void Open()
+    {
+        doorSprite.enabled = false;
+        open = true;
+        physicsCollider.enabled = false;
+    }
+
+    void Close()
+    {
+
+    }
+
+
+}

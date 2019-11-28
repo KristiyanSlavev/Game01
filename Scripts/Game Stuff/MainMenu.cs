24ffe21ca1a690a527b85c45ee78dba03c6a96ab
Kristiyan Slavev <kristiqnslavev97@gmail.com>
2019-11-28 10:35:02 +0200

    Main Menu

---
+++ Scripts/Game Stuff/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        //Add tutorial or intro here (e.g.)
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
